9360084ee316aa209eec0ee8c5d4a7c17193e8bc
abc <tjsrnjsdlqkq@naver.com>
2018-10-05 14:45:25 +0900

    나누기

--- caculator.java
+++ caculator.java
@@ -7,5 +7,8 @@ public class Calculator{
     } 
     public static int multi(int a, int b){
         return a * b;
-    }   
+    }  
+    public static divide (int a, int b){
+        return a / b;
+    }  
 }
