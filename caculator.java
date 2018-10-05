199fd2522e3f7a463a366ee2fe96f51c781ccc85
E7\DS <rlagmlgh98@naver.com>
2018-10-05 14:02:31 +0900

    multi()

--- caculator.java
+++ caculator.java
@@ -4,5 +4,8 @@ public class Calculator{
     }
     public static int minus(int a, int b){
         return a - b;
-    }    
+    } 
+    public static int multi(int a, int b){
+        return a * b;
+    }   
 }
